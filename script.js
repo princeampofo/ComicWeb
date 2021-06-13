2ecf5488bcf04ff67b742eacf52db94a61a72045
Prince Larbi Ampofo <pla252@nyu.edu>
2021-06-14 01:45:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+//Animation on scroll
 AOS.init({
   duration:2000,
   once:true,
+});
+
+
+
+//Background music
+const audio = document.querySelector("audio");
+let volbtn = document.querySelector(".vol-btn");
+let volicon = document.querySelector("#icon") ;
+
+volbtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    volicon.innerHTML = '<i class="fas fa-volume-up"></i>';
+    
+  } else {
+    audio.pause();
+    volicon.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
 });
